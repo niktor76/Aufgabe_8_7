f6e266872ae52e03369d087ce18d7f23e749e08f
viktor-nbubar <viktor.steck@gmail.com>
2020-05-26 12:58:42 +0200

    And so, it begins.

---
+++ cmake-build-debug/CMakeFiles/Aufgabe_8_7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Aufgabe_8_7"
+  "Aufgabe_8_7.pdb"
+  "CMakeFiles/Aufgabe_8_7.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aufgabe_8_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Aufgabe_8_7.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Aufgabe_8_7.
+# This may be replaced when dependencies are built.
